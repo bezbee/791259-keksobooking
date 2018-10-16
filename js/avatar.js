364f21a0a6cac5d84babaad6e1f24aed9cedc709
bezbee <zizu087@gmail.com>
2018-10-15 17:07:07 -0700

    fixed photo insertion

--- js/avatar.js
+++ js/avatar.js
@@ -3,27 +3,45 @@
 (function () {
 
   var FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
-  var fileChooser = document.querySelector('#images');
-  var div = document.querySelector('.ad-form__photo');
+  var photoInput = document.querySelector('#images');
+  var avatarInput = document.querySelector('#avatar');
+  var avatarPreview = document.querySelector('.ad-form-header__preview img');
+  var photoBlock = document.querySelector('.ad-form__photo-container');
+  var reader = new FileReader();
 
-  fileChooser.addEventListener('change', function () {
-    var file = fileChooser.files[0];
-    var fileName = file.name.toLowerCase();
+  var onAvatarLoad = function () {
+    avatarPreview.src = reader.result;
+  };
 
-    var matches = FILE_TYPES.some(function (it) {
-      return fileName.endsWith(it);
-    });
+  var onPhotoLoad = function () {
+    var img = document.createElement('img');
+    img.src = reader.result;
+    img.width = '70';
+    img.height = '70';
+    img.alt = 'Фотография пользователя';
+    var div = document.createElement('div');
+    div.className = 'ad-form__photo';
+    div.appendChild(img);
+    photoBlock.insertBefore(div, photoBlock.childNodes[3]);
+  };
 
-    if (matches) {
-      var reader = new FileReader();
+  var addPhoto = function (fileChooser, handler) {
 
-      reader.addEventListener('load', function () {
-        var newPhoto = document.createElement('img');
-        newPhoto.src = reader.result;
-        div.appendChild(newPhoto);
+    fileChooser.addEventListener('change', function () {
+      var file = fileChooser.files[0];
+      var fileName = file.name.toLowerCase();
+
+      var matches = FILE_TYPES.some(function (it) {
+        return fileName.endsWith(it);
       });
 
-      reader.readAsDataURL(file);
-    }
-  });
+      if (matches) {
+        reader.addEventListener('load', handler);
+        reader.readAsDataURL(file);
+      }
+    });
+  };
+
+  addPhoto(avatarInput, onAvatarLoad);
+  addPhoto(photoInput, onPhotoLoad);
 })();
